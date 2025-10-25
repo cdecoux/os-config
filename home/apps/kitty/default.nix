e421f0309c3158b69290bcd34a28848797a83dc6
Caleb DeCoux <cdecoux@protonmail.com>
2025-10-25 13:50:00 -0500

    finally symlink dotfiles instead of hm

--- home/apps/kitty/default.nix
+++ home/apps/kitty/default.nix
@@ -1,26 +1,20 @@
-{ config, pkgs, lib, ... }:
+{ inputs, config, pkgs, lib, ... }:
 
 {
-  programs.kitty = lib.mkForce {
-    enable = true;
-    # settings = {
-    #   confirm_os_window_close = 0;
-    #   dynamic_background_opacity = true;
-    #   enable_audio_bell = false;
-    #   mouse_hide_wait = "-1.0";
-    #   window_padding_width = 10;
-    #   background_opacity = "0.5";
-    #   background_blur = 5;
-    #   linux_display_server = "x11";
 
-    # };
+
+  home = {
+    packages = [
+      pkgs.kitty
+    ];
+    file = {
+      kitty = {
+        enable = true;
+        source = config.lib.file.mkOutOfStoreSymlink "${inputs.self}/dotfiles/kitty";
+        target = "${config.xdg.configHome}/kitty";
+        recursive = true;
+      };
+    };
+
   };
-
-  # xdg.configFile = {
-  #   kitty = {
-  #     source = config.lib.file.mkOutOfStoreSymlink ./files;
-  #     recursive = true;
-  #   };
-  # };
-
 }
