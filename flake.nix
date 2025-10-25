8668f963b5fd4af0365fb4c17a8610110cd7e45a
Caleb DeCoux <cdecoux@protonmail.com>
2025-10-24 23:07:26 -0500

    adding home manager

--- flake.nix
+++ flake.nix
@@ -3,13 +3,22 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    home-manager.url = github:nix-community/home-manager;
+    home-manager = {
+      # Follow corresponding `release` branch from Home Manager
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.fw-nixos = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; };
       modules = [ ./hosts/fw-nixos/configuration.nix ];
+    };
+
+    homeConfigurations.caleb = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      modules = [ ./users/caleb.nix ];
     };
   };
 }
